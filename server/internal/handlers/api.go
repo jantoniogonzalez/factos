6bdf30aee6020d39226f804bf1f3cb843cf2d1e7
Juan <jgonzalez020801@gmail.com>
2024-05-29 18:16:24 -0700

    1. Initial folder setup

---
+++ server/internal/handlers/api.go
@@ -0,0 +1,22 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
+)
+
+func Handlers(router chi.Router) {
+	router.Use(chiMiddleware.StripSlashes)
+
+	router.Route("/factos", func(router chi.Router) {
+		router.Get("/")     // GET all factos
+		router.Post("/new") // CREATE a new factos
+
+		router.Route("/{factoId}", func(router chi.Router) {
+			router.Get("/")    // GET facto with id
+			router.Put("/")    // EDIT an existing facto
+			router.Delete("/") // DELETE an existing facto
+
+		})
+	})
+}
