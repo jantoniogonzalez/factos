09b18d467478dccfe7926df846fc70b62b8d4347
Juan <jgonzalez020801@gmail.com>
2025-01-15 15:38:18 -0800

    Close modal

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,17 +1,27 @@
-const openNewFactos = document.querySelector("[newfactos-open-modal]")
-const newFactosModal = document.querySelector("[newfactos-modal]")
+const openNewFactos = document.querySelector("[newfactos-open-modal]");
+const newFactosModal = document.querySelector("[newfactos-modal]");
 //const closeNewFactos = document.querySelectorAll("[newfactos-close-modal]")
 
 openNewFactos.addEventListener("click", () => {
-    console.log("Opening modal")
-    newFactosModal.showModal()
-})
+    console.log("Opening modal");
+    newFactosModal.showModal();
+});
 
-const closeNewFactos = document.querySelectorAll(".newfactos-close-modal")
+const closeNewFactos = document.querySelector(".newfactos-close-modal");
+const exit = document.querySelector(".cancel-close-modal");
+const x_close = document.querySelector(".x-close-modal");
 
 closeNewFactos.addEventListener("click", () => {
-    console.log("Closing Modal")
-    newFactosModal.close()
-})
+    console.log("Closing Modal");
+    newFactosModal.close();
+});
 
+exit.addEventListener("click", () => {
+    console.log("Closing Modal");
+    newFactosModal.close();
+});
 
+x_close.addEventListener("click", () => {
+    console.log("Closing Modal");
+    newFactosModal.close();
+});
