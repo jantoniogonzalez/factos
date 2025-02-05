5f4068b7a001752dc3039aa07925e79c50f41d24
Juan <jgonzalez020801@gmail.com>
2025-02-05 09:52:25 -0800

    Fix closing modal

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,32 +1,31 @@
 const openNewFactos = document.querySelectorAll("[newfactos-open-modal]");
 const newFactosModal = document.querySelectorAll("[newfactos-modal]");
 //const closeNewFactos = document.querySelectorAll("[newfactos-close-modal]")
+
+const closeNewFactos = document.querySelectorAll(".newfactos-close-modal");
+const exit = document.querySelectorAll(".cancel-close-modal");
+const x_close = document.querySelectorAll(".x-close-modal");
 console.log("Running JS script");
 
 for (let i = 0; i < openNewFactos.length; i++) {
     openNewFactos.item(i).addEventListener("click", () => {
         console.log("Opening Modal");
         newFactosModal.item(i).showModal();
+        
+        closeNewFactos.item(i).addEventListener("click", () => {
+            console.log("Closing Modal");
+            newFactosModal.item(i).close();
+        });
+
+        exit.item(i).addEventListener("click", () => {
+            console.log("Closing Modal");
+            newFactosModal.item(i).close();
+        });
+
+        x_close.item(i).addEventListener("click", () => {
+            console.log("Closing Modal");
+            newFactosModal.item(i).close();
+        });
     })
+
 }
-  
-  
-
-const closeNewFactos = document.querySelector(".newfactos-close-modal");
-const exit = document.querySelector(".cancel-close-modal");
-const x_close = document.querySelector(".x-close-modal");
-
-closeNewFactos.addEventListener("click", () => {
-    console.log("Closing Modal");
-    newFactosModal.close();
-});
-
-exit.addEventListener("click", () => {
-    console.log("Closing Modal");
-    newFactosModal.close();
-});
-
-x_close.addEventListener("click", () => {
-    console.log("Closing Modal");
-    newFactosModal.close();
-});
