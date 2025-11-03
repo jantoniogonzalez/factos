984b5646ba6cd205336cdeeee46d4f0301776374
Juan <jgonzalez020801@gmail.com>
2025-11-02 23:05:51 -0800

    added new font and have idea for landing page

--- ui/web-client/src/pages/Home/Home.tsx
+++ ui/web-client/src/pages/Home/Home.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import styles from "./Home.module.css";
 import HeroImg from "../../assets/images/futbol.jpg";
 
+
 export const Home: React.FC = () => {
 	return(
 		<div className={styles.main}>
-      <div className={styles.image_container}>
-        <img src={HeroImg} alt="Hero Image"/>
-      </div>
       <div className={styles.text_container}>
-        <h1>FACTOS</h1>
+        <h1>FACT<span style={{"color": "var(--secondary)"}}>O</span>S</h1>
         <p>Predict results for upcoming games.<br/>Show your friends who knows football best.</p>
         
       </div>
